f2e77cb4810379d250f1058a2c87d254d85fc1f9
BaronSun <100346500+vvBaronSun@users.noreply.github.com>
2025-05-12 09:08:19 +0800

    Delete Dataset1 directory

--- Dataset1/External/tools/fitting/runSLM.m
+++
@@ -1,9 +0,0 @@
-function [slmOut,slmOnset,slmOffset,slmSlope] = runSLM(xdata,ydata,prescription)
-    slmOut = slmengine(xdata,ydata,prescription);
-    slmOnset = slmOut.knots(2);
-    if size(slmOut.knots,1) > 2
-        slmOffset = slmOut.knots(3);
-        slmSlope = (slmOut.coef(2)-slmOut.coef(3))/(slmOut.knots(2)-slmOut.knots(3));
-    else
-    end
-end
